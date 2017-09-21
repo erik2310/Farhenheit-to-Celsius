559cbf2dac1425c9b254639a5d9ea7df0334d705
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-09-21 14:23:34 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int farhenheit = 100;
+        double celsius = (5.0 / 9) * (farhenheit - 32);
+        System.out.println(farhenheit + " farhenheit er " + celsius + " celsius.");
+
+    }
+}
